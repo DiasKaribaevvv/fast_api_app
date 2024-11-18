c5aa77cc8bc07432f8dd72d488198d82b26fbad5
Karik <kar1baevvvdb@gmail.com>
2024-11-18 16:52:46 +0500

    CRUD operations by using sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fileinput import close
+from multiprocessing.sharedctypes import synchronized
 from select import error
 from typing import Optional
 
 from exceptiongroup import catch
 from fastapi import FastAPI,Response,status,HTTPException
-from fastapi.params import Body
+from fastapi.params import Body, Depends
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
@@ -34,7 +35,7 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True # if the user doesn't provide value it will be True OPTIONAL field
-    rating: Optional[int] = None # in this field we can not provide any information to this field it will be empty
+    #rating: Optional[int] = None # in this field we can not provide any information to this field it will be empty
 
 
 
@@ -74,15 +75,20 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    print(posts)
-    return {"data":posts}
+
+    post = Session.query(model.Post).all()
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    # print(posts)
+    return {"data":post}
 
 @app.get("/posts/{id}")
 async def get_specific_post(id: int,response:Response):
-    cursor.execute("""SELECT * FROM posts where id=%s""",(str(id)))
-    post = cursor.fetchone()
+
+    post = Session.query(model.Post).filter(model.Post.id == id).first()
+
+    # cursor.execute("""SELECT * FROM posts where id=%s""",(str(id)))
+    # post = cursor.fetchone()
     if not post:
         # response.status_code = status.HTTP_404_NOT_FOUND #changing to not found if answer is empty
         # return {'message': f'post with id {id} not found'}
@@ -97,9 +103,10 @@ async def get_latest_post():
 
 @app.post("/createpost",status_code=status.HTTP_201_CREATED)
 async def create_post(new_post:Post):
-    post = model.Post(title=new_post.title,content = new_post.content)
+    post = model.Post(**new_post.dict())
     Session.add(post)
     Session.commit()
+    Session.refresh(post)
 
 
     # cursor.execute(f"""INSERT INTO posts (title,content) VALUES (%s,%s) RETURNING *""",(new_post.title,new_post.content))
@@ -110,25 +117,42 @@ async def create_post(new_post:Post):
 
 @app.delete("/posts/delete/{id}",status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s returning *""",(str(id)))
-    delete_post_var = cursor.fetchone()
-    conn.commit()
-    if not delete_post_var:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"There is no post with id = {id}")
 
+    post = Session.query(model.Post).filter(model.Post.id == id)
+
+
+    # cursor.execute("""DELETE FROM posts WHERE id = %s returning *""",(str(id)))
+    # delete_post_var = cursor.fetchone()
+    # conn.commit()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"There is no post with id = {id}")
+    Session.delete(post)
+    Session.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/update/{id}")
-async def update_post(id: int,post : Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s where id = %s returning *""",(post.title,post.content,str(id)))
-    post = cursor.fetchone()
-    conn.commit()
-    if not post:
+async def update_post(id: int,new_post : Post):
+    post = Session.query(model.Post).filter(model.Post.id == id)
+    
+    if not post.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"There is no post with id = {id}")
+    
+    post.update(new_post.dict())
+    # print(new_post.dict())
+    Session.commit()
+    
+    
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s where id = %s returning *""",(post.title,post.content,str(id)))
+    # post = cursor.fetchone()
+    # conn.commit()
+    # if not post.first():
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"There is no post with id = {id}")
 
-    # post_dict = post.dict()
-    # post_dict["id"]=id
-    # my_posts[index]=post_dict
+    # post.update(new_post.dict(),synchronize_session=False)
 
+    # # post_dict = post.dict()
+    # # post_dict["id"]=id
+    # # my_posts[index]=post_dict
+    # Session.commit()
 
-    return {"data":post}
+    return {"data":"updated"}
--- app/model.py
+++ app/model.py
@@ -1,5 +1,8 @@
 from .database import Base
-from sqlalchemy import Column,Integer,String,Boolean
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+
 class Post(Base):
     __tablename__ = 'posts'
 
@@ -7,4 +10,5 @@ class Post(Base):
     title = Column(String,nullable=False)
     content = Column(String, nullable=False)
     published =Column(Boolean,default=True)
+    created_date = Column(TIMESTAMP(timezone=True),server_default=text('now()'),nullable=False)
 
